2e602b1d01c921b588fddd73037e729805b8446a
ahmadfma <56622688+wirajobs@users.noreply.github.com>
2021-12-14 15:01:59 +0800

    implement library

---
+++ app/src/main/java/com/wira/tugas_final_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wira.tugas_final_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
